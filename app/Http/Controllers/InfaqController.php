ed2d9df9295486750af61c975f1febf591596e0e
Ja'far Malik Ibrahim <92065895+JafarMalikIbrahim@users.noreply.github.com>
2024-10-23 21:22:32 +0700

    add db

---
+++ app/Http/Controllers/InfaqController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InfaqController extends Controller
+{
+     // Fungsi untuk menampilkan semua data infaq
+     public function index()
+     {
+         //
+     }
+ 
+     // Fungsi untuk menampilkan detail dari satu data infaq berdasarkan id
+     public function show($id)
+     {
+         //
+     }
+ 
+     // Fungsi untuk menambah data infaq
+     public function store(Request $request)
+     {
+         //
+     }
+ 
+     // Fungsi untuk menghapus data infaq
+     public function destroy($id)
+     {
+         //
+     }
+}
